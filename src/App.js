b1960b883bae6af48f514e5a161ffd87bb2cadc4
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-10-31 18:44:59 +0100

    Create footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Links from './components/links/Links';
 import Profile from './components/profile/Profile';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Links />
+      <Footer />
     </div>
   );
 }
