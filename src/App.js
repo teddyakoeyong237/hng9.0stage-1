9f25c5524e1742bc1d43449644f88ec2e2731324
teddyakoeyong237 <teddyakoeyong@gmail.com>
2022-10-31 18:13:07 +0100

    Created links component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Links from './components/links/Links';
 import Profile from './components/profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Links />
     </div>
   );
 }
